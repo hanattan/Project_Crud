5987e3269e4fdcc4dcf77f1989784ce761597b84
hanattan <Hanattanhuehuebr7@gmail.com>
2023-04-25 00:01:03 -0300

    Varsion 1.0.1.1

--- utilites/salvar_pessoas.php
+++ utilites/salvar_pessoas.php
@@ -1,5 +1,6 @@
 <?php
             switch (@$_REQUEST["acao"]){
+
                 case "cadastrar_pessoa": 
 
                 $nome = $_POST["nome"];
@@ -8,13 +9,21 @@
                 $sql = "INSERT INTO pessoa (nome, cpf) VALUES ('{$nome}','{$cpf}')";
                 $query = $connection->query($sql);
 
+                if($query == true){
+                echo "<script> alert('Cadastro de Pessoa feito com sucesso!')</script>";
+                echo "<script> location.href:'?page=home'</script>";
+                }else{
+                echo "<script> alert('Cadastro de Pessoa nâo realizado!')</script>";
+                echo "<script> location.href:'?page=home'</script>";
+                }
+
                 break;
 
                 case "editar_pessoa": 
-                    include("utilites\Listar_Contato.php");
+                    //include("utilites\Listar_Contato.php");
                 break;
                 case "incluir_pessoa": 
-                  include("utilites\salvar_pessoas.php");
+                  //include("utilites\salvar_pessoas.php");
               break;
     }
 ?>
